a845bf44f0b2cd576d1656472f0c3403d34da380
Caio Caminha <caminhacaiopro@gmail.com>
2025-05-22 20:25:43 -0300

    adding extension to AbstractR2DBCConfiguration

--- src/main/java/com/caiocaminha/javadailyexpenses/core/gateway/r2dbc/config/R2DBCConfiguration.java
+++ src/main/java/com/caiocaminha/javadailyexpenses/core/gateway/r2dbc/config/R2DBCConfiguration.java
@@ -4,10 +4,12 @@ package com.caiocaminha.javadailyexpenses.core.gateway.r2dbc.config;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 
 @Configuration
-public class R2DBCConfiguration {
+public class R2DBCConfiguration extends AbstractR2dbcConfiguration {
 
     R2DBCProperties properties;
 
@@ -19,6 +21,8 @@ public class R2DBCConfiguration {
 
     //TODO -> Investigate if this is the correct way of creating ConnectionFactory Bean
     //TODO -> Try Using ConnectionFactories.get(...)
+    @Override
+    @Bean
     public ConnectionFactory connectionFactory() {
         return new PostgresqlConnectionFactory(
                 PostgresqlConnectionConfiguration
